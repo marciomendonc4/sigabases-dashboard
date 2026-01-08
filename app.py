a1e6d9d1ceac8c9c275890d7c381dcbcee882059
Marcio Mendonça <92260114+marciomendonc4@users.noreply.github.com>
2026-01-08 09:16:03 -0300

    Initial Streamlit dashboard

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Dashboard TMA", layout="wide")
+
+st.title("Dashboard de Teste")
+st.write("Se você está vendo isso, o Streamlit está funcionando.")
